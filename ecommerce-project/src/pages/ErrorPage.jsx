8b7f8ba9ffa22dc2cfb990cd4c43d4d8b45a7d91
Odoh Stanley <stanleyodoh780@gmail.com>
2025-08-20 13:56:07 +0100

    Error message

---
+++ ecommerce-project/src/pages/ErrorPage.jsx
@@ -0,0 +1,19 @@
+import Header from "../components/Header";
+
+
+function ErrorPage() {
+  return (
+    <>
+    <title>404 Page Not Found</title>
+    <link rel="icon" type="image/svg+xml" href="home-favicon.png" />
+    <Header />
+
+
+      <div >
+        isois
+      </div>
+    </>
+  );
+}
+
+export default ErrorPage;

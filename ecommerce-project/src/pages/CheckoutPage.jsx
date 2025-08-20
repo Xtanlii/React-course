9d9e8ae922e33949a489d4c2ae292b5f6e094681
Odoh Stanley <stanleyodoh780@gmail.com>
2025-08-20 11:30:52 +0100

    Split checkout header into components

--- ecommerce-project/src/pages/CheckoutPage.jsx
+++ ecommerce-project/src/pages/CheckoutPage.jsx
@@ -1,31 +1,12 @@
-import './checkout-header.css'
-import './CheckoutPage.css'
+
+import CheckoutHeader from './checkout/CheckoutHeader';
+import './checkout/CheckoutPage.css';
 
 function CheckoutPage() {
-  return ( 
+  return (
     <>
       <title>Checkout</title>
-
-      <div className="checkout-header">
-        <div className="header-content">
-          <div className="checkout-header-left-section">
-            <a href="/">
-              <img className="logo" src="images/logo.png" />
-              <img className="mobile-logo" src="images/mobile-logo.png" />
-            </a>
-          </div>
-
-          <div className="checkout-header-middle-section">
-            Checkout (<a className="return-to-home-link"
-              href="/">3 items</a>)
-          </div>
-
-          <div className="checkout-header-right-section">
-            <img src="images/icons/checkout-lock-icon.png" />
-          </div>
-        </div>
-      </div>
-
+      <CheckoutHeader />
       <div className="checkout-page">
         <div className="page-title">Review your order</div>
 
@@ -183,38 +164,38 @@ function CheckoutPage() {
           </div>
 
           <div className="payment-summary">
-              <div className="payment-summary-title">
-                Payment Summary
-              </div>
+            <div className="payment-summary-title">
+              Payment Summary
+            </div>
 
-              <div className="payment-summary-row">
-                <div>Items (3):</div>
-                <div className="payment-summary-money">$42.75</div>
-              </div>
+            <div className="payment-summary-row">
+              <div>Items (3):</div>
+              <div className="payment-summary-money">$42.75</div>
+            </div>
 
-              <div className="payment-summary-row">
-                <div>Shipping &amp; handling:</div>
-                <div className="payment-summary-money">$4.99</div>
-              </div>
+            <div className="payment-summary-row">
+              <div>Shipping &amp; handling:</div>
+              <div className="payment-summary-money">$4.99</div>
+            </div>
 
-              <div className="payment-summary-row subtotal-row">
-                <div>Total before tax:</div>
-                <div className="payment-summary-money">$47.74</div>
-              </div>
+            <div className="payment-summary-row subtotal-row">
+              <div>Total before tax:</div>
+              <div className="payment-summary-money">$47.74</div>
+            </div>
 
-              <div className="payment-summary-row">
-                <div>Estimated tax (10%):</div>
-                <div className="payment-summary-money">$4.77</div>
-              </div>
+            <div className="payment-summary-row">
+              <div>Estimated tax (10%):</div>
+              <div className="payment-summary-money">$4.77</div>
+            </div>
 
-              <div className="payment-summary-row total-row">
-                <div>Order total:</div>
-                <div className="payment-summary-money">$52.51</div>
-              </div>
+            <div className="payment-summary-row total-row">
+              <div>Order total:</div>
+              <div className="payment-summary-money">$52.51</div>
+            </div>
 
-              <button className="place-order-button button-primary">
-                Place your order
-              </button>
+            <button className="place-order-button button-primary">
+              Place your order
+            </button>
           </div>
         </div>
       </div>

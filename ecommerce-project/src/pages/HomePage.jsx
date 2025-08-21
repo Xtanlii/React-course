526697d26ca40da492a7367a2c2d6a8b904c6ca7
Odoh Stanley <stanleyodoh780@gmail.com>
2025-08-21 12:03:31 +0100

    Generate html for the cart item

--- ecommerce-project/src/pages/HomePage.jsx
+++ ecommerce-project/src/pages/HomePage.jsx
@@ -3,6 +3,7 @@ import { useEffect, useState } from 'react';
 import Header from '../components/Header';
 import './HomePage.css';
 import Checkmark from '../assets/images/icons/checkmark.png';
+import { formatMoney } from '../utils/money';
 
 
 function HomePage({cart}) {
@@ -44,7 +45,7 @@ function HomePage({cart}) {
                 </div>
 
                 <div className="product-price">
-                  ${(product.priceCents / 100).toFixed(2)}
+                  ${formatMoney(product.priceCents) }
                 </div>
 
                 <div className="product-quantity-container">
